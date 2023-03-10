848f9cdc6f87c584a41257971099690eb5951778
semihozhan <semihozhan@yandex.com>
2023-03-10 21:41:04 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Xml;
+
+namespace RabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri= new Uri("amqps://kasdajfs:dejCFCnqfxsAJWQhSZyXXAVTlYsJsnRv@chimpanzee.rmq.cloudamqp.com/kasdajfs");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "rabbitmq first step";
+
+            var messageBoddy = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBoddy);
+
+            Console.WriteLine("mesaj iletildi");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kasdajfs:dejCFCnqfxsAJWQhSZyXXAVTlYsJsnRv@chimpanzee.rmq.cloudamqp.com/kasdajfs");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            var consumer = new  EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine(message.ToString());
+        }
+    }
+}
